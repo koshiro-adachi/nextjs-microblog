4492a401314ac85103f133d5265106d40d1bc281
adachi koshiro <koushiro54600404@gmail.com>
2022-08-07 18:33:31 +0900

    Layout.jsファイルを作成

--- pages/posts/firstpost.js
+++ pages/posts/firstpost.js
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function firstPost() {
   return (
     <div>
-        <Head>
-            <title>最初の投稿</title>
-        </Head>
+      <Head>
+        <title>最初の投稿</title>
+      </Head>
       <h1>最初の投稿</h1>
       <Link href="/">ホームへ戻る</Link>
     </div>
