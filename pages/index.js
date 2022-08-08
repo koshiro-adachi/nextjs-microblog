a22157cd1f2b1899878674aa5c097a7a9c1f05aa
adachi koshiro <koushiro54600404@gmail.com>
2022-08-08 11:16:38 +0900

    add thumbnails01-04

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,22 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
+import utilStyle from "../styles/utils.module.css";
 
 export default function Home() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <section className={utilStyle.headingMd}>
+        <p>フロントエンドエンジニアになる為修行中の身です。</p>
+      </section>
+
+      <div>
+        <article>
+          <Link>
+          <img src="" alt="" />
+          </Link>
+        </article>
+      </div>
+    </Layout>
+  );
 }
