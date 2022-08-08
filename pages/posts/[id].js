cf205b607b2ec317859dcee0f0df551f9f12ac4d
adachi koshiro <koushiro54600404@gmail.com>
2022-08-08 21:47:51 +0900

    getStaticPathsためのgetStaticProps

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,4 +1,18 @@
 import Layout from "../../components/Layout";
+import { getAllPostIds } from "../../lib/post";
+
+export async function getStaticPaths() {
+    const paths=getAllPostIds();
+
+    return{
+        paths,/*paths:pathsなので省略している*/
+        fallback:false
+    };
+}
+
+export function getStaticProps(props){
+    const {params} =props;
+}
 
 export default function Post() {
     return (
