cb799f9cc9e4b3a68fb09ac27372ed0d40ea7e72
adachi koshiro <koushiro54600404@gmail.com>
2022-08-08 18:48:57 +0900

    [id].js動的ルーティング設定

--- components/Layout.js
+++ components/Layout.js
@@ -12,7 +12,7 @@ function Layout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img src="images/sibaDog.jpg" alt="profile_image" className={utilStyles.borderCircle}/>
+        <img src="/images/sibaDog.jpg" alt="profile_image" className={utilStyles.borderCircle}/>
         <h1 className={utilStyles.heading2Xl} >{name}</h1>
       </header>
       <main>{children}</main>
---
+++ pages/posts/[id].js
@@ -0,0 +1,7 @@
+import Layout from "../../components/Layout";
+
+export default function Post() {
+    return (
+        <Layout>動的ルーティング設定</Layout>
+    );
+}
